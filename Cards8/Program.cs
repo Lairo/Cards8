3399b8ece67715786c4fc943d1c43d8563321d83
chajus1 <chajus1@googlemail.com>
2023-07-03 11:53:49 +0100

    expanding

--- Cards8/Program.cs
+++ Cards8/Program.cs
@@ -10,7 +10,32 @@ namespace Cards8
             Console.WriteLine(aceOfSpades.Name);
             Card duchessOfRoses = new Card("Duchess", Suits.Diamonds);
             Card dukeOfOxen = new Card("Duke", Suits.Hearts);
-            Console.WriteLine(duchessOfRoses.Name + "\n" + dukeOfOxen.Name);
+            Console.WriteLine(duchessOfRoses.Name + "\n" + dukeOfOxen.Name + "\n");
+
+            Suits mySuit = Suits.Diamonds;
+
+            Suits[] myVals = new Suits[3] { Suits.Spades, Suits.Clubs, mySuit };
+
+            foreach (Suits val in myVals)
+            {
+                Console.WriteLine(val);
+            } 
+
+            void IsAHeart(Suits suit)
+            {
+                if (suit == Suits.Hearts)
+                {
+                    Console.WriteLine("You pulled a heart!");
+                }
+                else
+                {
+                    Console.WriteLine($"You didn't pull a heart: {suit}");
+                }
+            }
+
+            IsAHeart(Suits.Oxen);
+
+
         }
     }
 }
